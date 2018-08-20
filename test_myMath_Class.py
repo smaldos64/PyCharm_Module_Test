7289d93a7ecfe9d2601695376ca79e147ce5ba6f
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-08-20 13:01:21 +0200

    Adderet TestCase.

--- test_myMath_Class.py
+++ test_myMath_Class.py
@@ -14,6 +14,19 @@ class TestMyMath_Class(unittest.TestCase):
         Discriminant = MyMath_Class.calculate_discriminant(2, 2, 2)
         self.assertEqual(Discriminant, -12)
 
+    def test_calculate_discriminant_self(self):
+        Math_Obj = MyMath_Class()
+
+        Discriminant = Math_Obj.calculate_discriminant_self(4, 10, 4)
+        self.assertEqual(Discriminant, 36)
+
+        Discriminant = Math_Obj.calculate_discriminant_self(2, 4, 2)
+        self.assertEqual(Discriminant, 0)
+
+        Discriminant = Math_Obj.calculate_discriminant_self(2, 2, 2)
+        self.assertEqual(Discriminant, -12)
+
+
 
 if __name__ == '__main__':
     unittest.main()
